4adf09f40c15bc71910a33d16a3cdb8af7b828f3
Shaun Xu <jfarrio@gmail.com>
2025-04-25 16:36:10 +0800

    .

--- bge-m3.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
-
-path = "/Users/shaunxu/huggingface/bge-m3"
-tokenizer = AutoTokenizer.from_pretrained(path)
-model = AutoModel.from_pretrained(path)
-
-print(model)
